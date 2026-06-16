b12ca01cc19ad29d459cd2f41e4f3e24c5afd7ac
tmnlbus <trustmedianl@gmail.com>
2026-06-16 06:18:28 +0200

    Delete src/main/java/net/runelite/client/plugins/microbot/vardorvis directory

--- src/main/java/net/runelite/client/plugins/microbot/vardorvis/VardorvisConfig.java
+++
@@ -1,8 +0,0 @@
-package net.runelite.client.plugins.microbot.vardorvis;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-
-@ConfigGroup("Vardorvis")
-public interface VardorvisConfig extends Config {
-}
--- src/main/java/net/runelite/client/plugins/microbot/vardorvis/enums/StatePOH.java
+++
@@ -1,7 +0,0 @@
-package net.runelite.client.plugins.microbot.vardorvis.enums;
-
-public enum StatePOH {
-    UNKNOWN,
-    REJUVENATION,
-    GRAND_EXCHANGE
-}
